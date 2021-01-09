5054fdb5f843233014fd32f134305d03c7eb3a9e
Seth Todd <seth.todd@outlook.com>
2021-01-09 13:58:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z\d 0-9-']+/g,'');
+  }
+
+  static titleize(string) {
+    let ar = string.split(' ');
+    let firstWord = ar.shift();
+    let firstWordCap = firstWord.charAt(0).toUpperCase() + firstWord.slice(1)
+    let shortPhrase = ar.map((word) => {
+      if (['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'].indexOf(word) === -1) {
+        return word[0].toUpperCase() + word.substring(1);
+      } else {
+        return word
+      }
+    }).join(" ");
+    let phrase = `${firstWordCap + " " + shortPhrase}`;
+    return phrase
+  }
 }
